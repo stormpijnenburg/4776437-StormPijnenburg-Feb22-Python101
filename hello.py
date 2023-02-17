08a3aff6ff52ad7b7d9873124e92ece5a012e8cb
i502384 <i502384@fhict.nl>
2023-02-17 10:50:00 +0100

    tested some things with your pwp

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("hello, world")
+list_of_times = [12, 53, 32, "text"]
+
+for counter in range(0, 20, 2):
+    print(counter)
+
+for index_in_list, item in enumerate(list_of_times):
+    print(f"index: {index_in_list}\tValue: {item}")
+    if index_in_list < len(list_of_times) -1:
+        list_of_times[index_in_list+1]
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        
